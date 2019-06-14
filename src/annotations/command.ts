50473e8cd82d8c1c58f34f5717f7053bbbbf8020
WittBulter <lambdas.dev@gmail.com>
2019-06-14 13:47:48 +0800

    fix: rename

--- src/annotations/command.ts
+++ src/annotations/command.ts
@@ -3,13 +3,13 @@ import * as validator from '../utils/validator'
 import { CommandParams } from '../interfaces'
 import { handlers, metadata } from '../constants/metadata'
 
-const commandFactory = (commandOptions: CommandParams) => target => {
-  validator.requireKey(commandOptions.name, 'name')
+const commandFactory = (commandParams: CommandParams) => target => {
+  validator.requireKey(commandParams.name, 'name')
   
-  Reflect.defineMetadata(metadata.COMMAND_IDENTIFIER, commandOptions || {}, target)
+  Reflect.defineMetadata(metadata.COMMAND_IDENTIFIER, commandParams || {}, target)
   Reflect.defineMetadata(metadata.HANDLER_IDENTIFIER, handlers.COMMAND, target)
   return target
 }
 
-export const Command = (commandOptions: CommandParams) => commandFactory(commandOptions)
+export const Command = (commandParams: CommandParams) => commandFactory(commandParams)
 
